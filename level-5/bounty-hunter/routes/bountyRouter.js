ec4467ec794d130dae9cb66cd20485e6cff3ad51
Janina Alvarez <janinatalvarez@ucla.edu>
2023-04-30 14:39:46 -0700

    Bounty hunter pt 2

--- level-5/bounty-hunter/routes/bountyRouter.js
+++ level-5/bounty-hunter/routes/bountyRouter.js
@@ -3,9 +3,9 @@ const express = require('express');
 const bountyRouter = express.Router();
 
 const bounties = [
-    {firstName: 'Sheeve', lastName: 'Palpatine', living: true, type: 'sith', id: uuidv4()},
-    {firstName: 'Anakin', lastName: 'Skywalker', living: true, type: 'jedi', id: uuidv4()},
-    {firstName: 'Qui Gon', lastName: 'Gin', living: false, type: 'jedi', id: uuidv4()}
+    {firstName: 'Sheeve', lastName: 'Palpatine', living: true, type: 'sith', _id: uuidv4()},
+    {firstName: 'Anakin', lastName: 'Skywalker', living: true, type: 'jedi', _id: uuidv4()},
+    {firstName: 'Qui Gon', lastName: 'Gin', living: false, type: 'jedi', _id: uuidv4()}
 ]
 
 bountyRouter.route('/')
@@ -14,9 +14,29 @@ bountyRouter.route('/')
     })
     .post((req, res)=>{
         const newBounty = req.body
-        newBounty.id = uuidv4();
+        newBounty._id = uuidv4();
         bounties.push(newBounty);
         res.send(`Successfully added ${newBounty.firstName}`)
+    })
+
+bountyRouter.route('/:bountyId')
+    .get((req,res)=>{
+        const bountyId = req.params.bountyId;
+        const foundBounty = bounties.find(bounty=>bounty._id === bountyId);
+        res.send(foundBounty);
+    })
+    .put((req, res)=>{
+        const bountyId = req.params.bountyId;
+        const updatedBountyData = req.body;
+
+        const bountyToEdit = bounties.findIndex(bounty=>bountyId===bounty._id);
+        res.send(Object.assign(bounties[bountyToEdit], updatedBountyData));
+    })
+    .delete((req, res)=>{
+        const bountyId = req.params.bountyId;
+        const bountyToDelete = bounties.findIndex(bounty=>bountyId===bounty._id);
+        bounties.splice(bounties[bountyToDelete],1);
+        res.send(`Sucessfully deleted item`);
     })
 
 module.exports = bountyRouter;
