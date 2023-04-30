efbc336ad9e827c2b692f36caa7f9a1adb006342
Janina Alvarez <janinatalvarez@ucla.edu>
2023-04-30 14:42:49 -0700

    add in old project

---
+++ level-3/first-react/index.js
@@ -0,0 +1,12 @@
+const navbar = (
+        <nav>
+            <h1>My First React Website</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+
+ReactDOM.render(navbar, document.getElementById("root"));
